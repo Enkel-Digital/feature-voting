fd07eaca4c34eb678907ca76a0222937396d5239
Jaime <jaimeloeuf@gmail.com>
2021-07-30 22:46:57 +0800

    app: Re-formatted Main.elm using elm-format.

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,5 @@
 module Main exposing (..)
 
-
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
@@ -13,7 +12,7 @@ import Html.Events exposing (onInput)
 
 main : Program () Model Msg
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
 
 
 
@@ -21,41 +20,40 @@ main =
 
 
 type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  
-  , features : List String
-  }
+    { name : String
+    , password : String
+    , passwordAgain : String
+    , features : List String
+    }
 
 
 init : Model
 init =
-  Model "" "" "" ["faster loading time", "search for item using itemID too"]
+    Model "" "" "" [ "faster loading time", "search for item using itemID too" ]
+
+
+
 -- Scaffolded values to test UI
-
-
-
 -- UPDATE
 
 
 type Msg
-  = Name String
-  | Password String
-  | PasswordAgain String
+    = Name String
+    | Password String
+    | PasswordAgain String
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Name name ->
-      { model | name = name }
+    case msg of
+        Name name ->
+            { model | name = name }
 
-    Password password ->
-      { model | password = password }
+        Password password ->
+            { model | password = password }
 
-    PasswordAgain password ->
-      { model | passwordAgain = password }
+        PasswordAgain password ->
+            { model | passwordAgain = password }
 
 
 
@@ -64,30 +62,31 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h1 [] [ text "All features" ]
-    , viewFeatures model.features
-    , viewInput "text" "Name" model.name Name
-    , viewInput "password" "Password" model.password Password
-    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
-    , viewValidation model
-    ]
+    div []
+        [ h1 [] [ text "All features" ]
+        , viewFeatures model.features
+        , viewInput "text" "Name" model.name Name
+        , viewInput "password" "Password" model.password Password
+        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , viewValidation model
+        ]
 
 
 viewFeatures : List String -> Html msg
 viewFeatures features =
-  ol []
-    (List.map (\feature -> li [] [ text feature ]) features)
+    ol []
+        (List.map (\feature -> li [] [ text feature ]) features)
 
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
 viewInput t p v toMsg =
-  input [ type_ t, placeholder p, value v, onInput toMsg ] []
+    input [ type_ t, placeholder p, value v, onInput toMsg ] []
 
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  if model.password == model.passwordAgain then
-    div [ style "color" "green" ] [ text "OK" ]
-  else
-    div [ style "color" "red" ] [ text "Passwords do not match!" ]
+    if model.password == model.passwordAgain then
+        div [ style "color" "green" ] [ text "OK" ]
+
+    else
+        div [ style "color" "red" ] [ text "Passwords do not match!" ]
